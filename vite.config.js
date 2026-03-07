e55038ce86e09e9de87dfffb0a55b4f9a1a5b751
Hampuhh <43082280+Hampuhh@users.noreply.github.com>
2026-03-07 19:55:38 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Solo-Leveling-Fitness/', /* <-- ESTE ES EL CAMBIO CLAVE */
+  base: '/Solo-Leveling-Fitness/', 
 })
