70233d846f3fddd87faf6647e0aae9dd73939c63
Tu NombreHampuhh <ampa.navarrete.sm@gmail.com>
2026-03-07 19:39:01 -0300

    Configuración lista para subir a producción

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mi-sistema-fitness/', /* ¡ESTA LÍNEA ES CRUCIAL! Pon el nombre exacto de tu repositorio entre las barras */
 })
