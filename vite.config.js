34e8795c8559d248fb8e00a431d46180fc270778
Hampuhh <43082280+Hampuhh@users.noreply.github.com>
2026-03-07 19:49:49 -0300

    Change base URL for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mi-sistema-fitness/', /* ¡ESTA LÍNEA ES CRUCIAL! Pon el nombre exacto de tu repositorio entre las barras */
+  base: '/Solo-Leveling-Fitness/', /* <-- ESTE ES EL CAMBIO CLAVE */
 })
